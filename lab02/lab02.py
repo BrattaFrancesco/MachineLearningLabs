65e3fd9d52cb2ad722fd9a298ac0a65ca16fb6f7
Francesco Bratta <fra.bra00@gmail.com>
2024-03-23 18:19:13 +0100

    Finish lab + start project

--- lab02/lab02.py
+++ lab02/lab02.py
@@ -1,6 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def mcol(v):
+    return v.reshape((v.size, 1))
+
 def load(fileName):
     print("Loading dataset...")
     f=open(fileName, 'r')
@@ -30,15 +33,8 @@ def load(fileName):
         line = f.readline()
 
     return np.hstack(datasetList), np.array(labelList)
-        
-if __name__ == '__main__':
-    fileName = "dataset/iris.csv"
-    #D[0]-> sepal lenght
-    #D[1]-> sepal width
-    #D[2]-> petal lenght
-    #D[3]-> petal width
-    D, L = load(fileName)
 
+def plotHistograms(D, L):
     #set te columns with iris setosa to true, others to false; select from the dataset only iris setosa
     #setosa
     D0 = D[:, L == 0]
@@ -62,7 +58,7 @@ if __name__ == '__main__':
     plt.xlabel("Sepal widht")
     plt.legend()
     plt.figure()
-
+    
     #petal lenght
     plt.hist(D0[2,:], density=True, alpha=0.5, label="Setosa")
     plt.hist(D1[2,:], density=True, alpha=0.5, label="Versicolor")
@@ -72,9 +68,67 @@ if __name__ == '__main__':
     plt.figure()
 
     #petal widht
-    plt.hist(D0[2,:], density=True, alpha=0.5, label="Setosa")
-    plt.hist(D1[2,:], density=True, alpha=0.5, label="Versicolor")
-    plt.hist(D2[2,:], density=True, alpha=0.5, label="Virginica")
+    plt.hist(D0[3,:], density=True, alpha=0.5, label="Setosa")
+    plt.hist(D1[3,:], density=True, alpha=0.5, label="Versicolor")
+    plt.hist(D2[3,:], density=True, alpha=0.5, label="Virginica")
     plt.xlabel("Petal widht")
     plt.legend()
     plt.show()
+
+def plotScatters(D, L):
+    #set te columns with iris setosa to true, others to false; select from the dataset only iris setosa
+    #setosa
+    D0 = D[:, L == 0]
+    #versicolor
+    D1 = D[:, L == 1]
+    #virginica
+    D2 = D[:, L == 2]
+
+    for i in range(4):
+        for j in range(4):
+            if(i != j):
+                plt.figure()
+                plt.scatter(D0[i, :], D0[j, :], label="Setosa")
+                plt.scatter(D1[i, :], D1[j, :], label="Versicolor")
+                plt.scatter(D2[i, :], D2[j, :], label="Virginica")
+                plt.legend()
+        plt.show()
+
+def datasetMean(D):
+    mu = mcol(D.mean(1))
+    DC = D - mu
+    return DC, mu
+
+def covarianceMatrix(D, mu):
+    C = ((D - mu) @ (D - mu).T) / float(D.shape[1])
+    return C
+
+if __name__ == '__main__':
+    fileName = "dataset/iris.csv"
+    #D[0]-> sepal lenght
+    #D[1]-> sepal width
+    #D[2]-> petal lenght
+    #D[3]-> petal width
+    D, L = load(fileName)
+
+    DC, mu = datasetMean(D)
+    #plotHistograms(DC, L)
+    #plotScatters(DC, L)
+    C = covarianceMatrix(D, mu)
+    print(C)
+
+    var = D.var(1)
+    std = D.std(1)
+    print("Var: ", var, "\nStd: ", std)
+
+    for cls in [0,1,2]:
+        print("Class: ", cls)
+        classD = D[:, L == cls]
+        classDC, classMu = datasetMean(classD)
+
+        print("Mean: ", classMu)
+        print("Covariance matrix: \n", classDC)
+
+        var = classD.var(1)
+        std = classD.std(1)
+        print("Var: ", var, "\nStd: ", std)
