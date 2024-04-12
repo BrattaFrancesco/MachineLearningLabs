7c402e9ca4c6232aaf6abd39d5b4463a22c09538
Francesco Bratta <fra.bra00@gmail.com>
2024-04-12 16:15:54 +0200

    Started lab04, finished ex01

---
+++ lab04/lab04.py
@@ -0,0 +1,41 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def vrow(v):
+    return v.reshape((1, v.size))
+
+def mcol(v):
+    return v.reshape((v.size, 1))
+
+def logpdf_GAU_ND(X, mu, C):
+    Y = []
+    N = X.shape[0]
+
+    for x in X.T:
+        x = mcol(x)
+
+        const = N*np.log(2*np.pi)
+
+        logC = np.linalg.slogdet(C)[1]
+
+        mult = np.dot(np.dot( (x-mu).T, np.linalg.inv(C)), (x-mu))[0, 0]
+
+        Y.append(-0.5*(const+logC+mult))
+    return np.array(Y)
+
+if __name__ == "__main__":
+    XND = np.load('lab04/solutions/XND.npy')
+    mu = np.load('lab04/solutions/muND.npy')
+    C = np.load('lab04/solutions/CND.npy')
+    pdfSol = np.load('lab04/solutions/llND.npy')
+    pdfGau = logpdf_GAU_ND(XND, mu, C)
+    print(np.abs(pdfSol - pdfGau).max())
+
+    plt.figure()
+    XPlot = np.linspace(-8, 12, 1000)
+    m = np.ones((1,1)) * 1.0
+    C = np.ones((1,1)) * 2.0
+    plt.plot(XPlot.ravel(), np.exp(logpdf_GAU_ND(vrow(XPlot), m, C)))
+    plt.show()
+
+
